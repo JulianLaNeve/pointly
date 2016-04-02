b77eb6fc22a7b38cc88178c44f4bf0940250ef95
Julian LaNeve <lanevejulian@gmail.com>
2016-04-02 18:53:12 -0500

    Fix pratik

--- client/signup/signup.js
+++ client/signup/signup.js
@@ -5,5 +5,20 @@ Template.signup.events({
 		var email = $('#email').val();
 		var password = $('#password').val();
 		var confirm = $('#confirm').val();
+		if (password != confirm) {
+			return;
+		}
+
+		Accounts.createUser({
+			username: first + last,
+			email: email,
+			password: password,
+			profile: {
+				first: first,
+				last: last
+			}
+		});
+
+		Router.go('/add')
 	}
 })
