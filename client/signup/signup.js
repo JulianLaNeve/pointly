d12693d25a0fe998f79f4c6c9cf1739b97b27819
Julian LaNeve <lanevejulian@gmail.com>
2016-04-03 01:36:42 -0500

    Gross hack because Pratik doesn't know how to code

--- client/signup/signup.js
+++ client/signup/signup.js
@@ -5,7 +5,28 @@ Template.signup.events({
 		var email = $('#email').val();
 		var password = $('#password').val();
 		var confirm = $('#confirm').val();
-		if (password != confirm) {
+		var isAdmin = $('#admin').is(":checked");
+		if (!isAdmin) {
+			var adminEmail = $('#adminEmail').val();
+		}
+
+		if (!first) {
+			Materialize.toast("You need to enter a first name", 2000);
+			return;
+		} else if (!last) {
+			Materialize.toast("You need to enter a last name", 2000);
+			return;
+		} else if (!email) {
+			Materialize.toast("You need to enter an email", 2000);
+			return;
+		} else if (!password) {
+			Materialize.toast("You need to enter a password", 2000);
+			return;
+		} else if (!confirm) {
+			Materialize.toast("You need to confirm your password", 2000);
+			return;
+		} else if (password != confirm) {
+			Materialize.toast("Your passwords don't match", 2000);
 			return;
 		}
 
@@ -16,19 +37,50 @@ Template.signup.events({
 			if (err) {
 				console.log(err);
 			} else {
-				Accounts.createUser({
-					username: first + last,
-					email: email,
-					password: password,
-					profile: {
-						first: first,
-						last: last,
-						imageId: fileObj._id
-					}
-				});
+				if (isAdmin) {
+					Accounts.createUser({
+						username: first + last,
+						email: email,
+						password: password,
+						profile: {
+							first: first,
+							last: last,
+							role: 'admin',
+							imageId: fileObj._id
+						}
+					});
+				} else if (!isAdmin) {
+					Accounts.createUser({
+						username: first + last,
+						email: email,
+						password: password,
+						profile: {
+							first: first,
+							last: last,
+							role: 'user',
+							adminEmail: adminEmail,
+							imageId: fileObj._id
+						}
+					});
+				} else {
+					Materialize.toast('error', 2000)
+				}
 			};
 		});
 
 		Router.go('/dashboard')
+	},
+	'change #admin': function(e) {
+		Session.set('admin', e.target.checked);
 	}
-})
+});
+
+Template.signup.helpers({
+	'isAdmin': function() {
+		return Session.get('admin');
+	}
+});
+
+Template.signup.onRendered = function() {
+	Session.set('admin', true);
+}
