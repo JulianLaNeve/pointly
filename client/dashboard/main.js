cfb18f50ccf5210ac754784ca3e423ddae0526ba
PratikKelkar <pratikkelkar11@gmail.com>
2016-04-03 00:28:54 -0500

    stuff

--- client/dashboard/main.js
+++ client/dashboard/main.js
@@ -6,3 +6,13 @@ Template.dashboard.helpers({
 		return true;
 	}
 })
+
+Template.adminDashboard.helpers({
+	'name': function() {
+		return Meteor.user().profile.first + " " + Meteor.user().profile.last;
+	},	
+
+	'proPic': function() {
+		return Images.findOne(Meteor.user().profile.imageId).url();
+	}
+})
