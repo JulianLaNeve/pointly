f9f3a75c2521fd4dc2484e8a350dbb45b0deb712
sacheth25 <Sacheth@Sacheths-MacBook-Pro.local>
2016-04-03 06:57:23 -0500

    ,jhaejdhvsd,icv

---
+++ client/home/home.js
@@ -0,0 +1,10 @@
+Template.home.rendered = function() {
+	$("#typed").typed({
+	    strings: ["complete tasks.", "increase productivity.", "earn rewards."],
+	    typeSpeed: 50,
+	    startDelay: 1000,
+	    backSpeed: 25,
+	    backDelay: 1000,
+	    showCursor: false,
+	  });
+}
