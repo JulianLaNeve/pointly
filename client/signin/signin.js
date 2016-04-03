7507ce78e377c6283ae3676b4af6e966494b7b6c
Julian LaNeve <lanevejulian@gmail.com>
2016-04-02 19:04:41 -0500

    this doesn't really make things faster, but I tried

---
+++ client/signin/signin.js
@@ -0,0 +1,10 @@
+Template.signin.events({
+	'click .btn': function(e) {
+		var email = $('#email').val();
+		var password = $('#password').val();
+
+		Meteor.loginWithPassword(email, password)
+
+		Router.go('dashboard');
+	}
+})
